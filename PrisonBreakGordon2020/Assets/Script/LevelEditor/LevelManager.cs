541a648774e27839fbafc96f091f82caeeee3740
Datguypotato <g.cheung@sintlucasedu.nl>
2020-02-03 16:19:23 +0100

    Started raycasting for spawning object
    I am going to leave moving camera for now as it is not a priority

---
+++ PrisonBreakGordon2020/Assets/Script/LevelEditor/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    Vector2 xBoundaries;
+    Vector2 zBoundaries;
+
+
+    private void Start()
+    {
+        // calculating the bounda
+        Collider box = GetComponent<BoxCollider>();
+        xBoundaries = new Vector2(box.bounds.min.x, box.bounds.max.x);
+        zBoundaries = new Vector2(box.bounds.min.z, box.bounds.max.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            FindObjectOfType<PlayerFollow>().SetupNewLevel(this.transform, xBoundaries, zBoundaries);
+        }
+    }
+}
