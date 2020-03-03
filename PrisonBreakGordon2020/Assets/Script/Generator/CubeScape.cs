efe7cd25b608e00854d3f830e7897d90a3e0a998
Datguypotato <g.cheung@sintlucasedu.nl>
2020-03-03 12:32:19 +0100

    generated terrain

---
+++ PrisonBreakGordon2020/Assets/Script/Generator/CubeScape.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScape : MonoBehaviour
+{
+    public GameObject prefab; //= GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+    public float maxHeight = 1f;
+    public float minHeight = 0f;
+
+    public int gridSize = 5;
+    [Tooltip("The lower the number the more difference in height")]
+    public float detail = 5.0f;
+
+    private void Start()
+    {
+        ProceduralGenerator.instance.SetSeet(10);
+        Generate();
+
+        
+    }
+
+    public void Generate()
+    {
+        if(ProceduralGenerator.instance != null)
+        {
+            for (int x = 0; x < gridSize; x++)
+            {
+                for (int z = 0; z < gridSize; z++)
+                {
+                    //float r = Random.Range(minHeight, maxHeight);
+                    float perlinX = x / detail + ProceduralGenerator.instance.GetPerlinSeed();
+                    float perlinZ = z / detail + ProceduralGenerator.instance.GetPerlinSeed();
+                    float r = (Mathf.PerlinNoise(perlinX, perlinZ) -minHeight) * maxHeight;
+
+
+                    Vector3 randomPos = new Vector3(x, r, z) + transform.position;
+                    GameObject temp = Instantiate(prefab, randomPos, Quaternion.identity, ProceduralGenerator.instance.transform);
+
+                    // color
+                    float height = 1 - temp.transform.position.y / maxHeight;
+                    temp.GetComponent<MeshRenderer>().material.color = new Color(height, height, height);
+                }
+            }
+
+        }
+        else
+        {
+            Debug.LogError("There is no ProceduralGenerator class in this scene");
+        }
+        
+    }
+}
---
+++ PrisonBreakGordon2020/Assets/Script/Generator/ProceduralGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGenerator : MonoBehaviour
+{
+    public static ProceduralGenerator instance;
+
+    private int seed;
+    private float perlinSeed;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void SetSeet(int newSeed)
+    {
+        seed = newSeed;
+        Random.InitState(seed);
+        perlinSeed = Random.Range(-10000f, 10000f);
+    }
+
+    public float GetPerlinSeed()
+    {
+        return perlinSeed;
+    }
+}
